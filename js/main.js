b35061bf46403556f0fe43d131d5549f582ee97c
Alex Tomala <actomala@uwaterloo.ca>
2016-09-09 23:03:36 -0400

    Scrolling fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $( document ).ready(function(){
-    $('.scrollspy').scrollSpy({offset: 10});
+	/*$('.navbar-fixed a').on('click', function(event) {
+		event.preventDefault();
+		$($(this).attr('href'))[0].scrollIntoView();
+		scrollBy(0, -48);
+	});*/
+	var options = {
+			throttle: 100,
+			scrollOffset: 40 
+    };
+	$('.scrollspy').scrollSpy(options);
 	$(".button-collapse").sideNav();
 	$('.download-resume').on('click', function() {
  	 ga('send', 'event', 'button', 'click', 'Download Resume');
