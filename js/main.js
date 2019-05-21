c9d2d59df74921d57eb4110cf2b4293587a62280
Alex Tomala <alex@atomala.com>
2019-05-21 13:47:19 -0400

    Few changes to the JS code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-$( document ).ready(function(){
-	/*$('.navbar-fixed a').on('click', function(event) {
-		event.preventDefault();
-		$($(this).attr('href'))[0].scrollIntoView();
-		scrollBy(0, -48);
-	});*/
-	var options = {
-			throttle: 100,
-			scrollOffset: 40 
-    };
-	$('.scrollspy').scrollSpy(options);
-	$(".button-collapse").sideNav();
-	$('.download-resume').on('click', function() {
- 	 ga('send', 'event', 'button', 'click', 'Download Resume');
-	});
-})
+Array.from(document.getElementsByClassName("download-resume")).forEach(function(element) {
+  element.addEventListener('click', function() {
+    ga('send', 'event', 'button', 'click', 'Download Resume');
+  });
+});
